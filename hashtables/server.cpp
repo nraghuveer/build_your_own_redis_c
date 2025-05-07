3dd711b54a7e28d973a600c0bee11102538bdf44
Raghuveer N <narahar2@uwm.edu>
2025-05-07 07:12:00 -0700

    finish the hashtable implementation

--- hashtables/server.cpp
+++ hashtables/server.cpp
@@ -1,114 +1,87 @@
+#include "server.hpp"
+#include "hashtable.hpp"
 #include <assert.h>
 #include <cstdint>
 #include <cstring>
 #include <errno.h>
 #include <fcntl.h>
-#include <map>
 #include <netinet/ip.h>
 #include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
-#include "hashtable.hpp"
 #include <sys/socket.h>
 #include <unistd.h>
 #include <vector>
 
-constexpr size_t k_max_msg = 32 << 20;
-
-#define container_of(ptr, T, member) \
-  ((T *)((char *)ptr - offsetof(T, member)))
-
-static struct
-{
+static struct {
   HMap db;
 } g_data;
 
-struct Entry
-{
-  struct HNode node;
-  std::string key;
-  std::string val;
-};
-
-static bool entry_eq(HNode *x, HNode *y)
-{
-  Entry *ex = container_of(x, struct Entry, node);
-  Entry *ey = container_of(y, struct Entry, node);
-  return ex->key == ey->key;
+static void do_request(std::vector<std::string> &cmd, Response &out) {
+  if (cmd.size() == 2 && cmd[0] == "get") {
+    do_get(cmd, out);
+  } else if (cmd.size() == 3 && cmd[0] == "set") {
+    do_set(cmd, out);
+  } else if (cmd.size() == 2 && cmd[0] == "del") {
+    do_del(cmd, out);
+  } else {
+    out.status = RES_ERR;
+  }
+  return;
 }
 
-enum
-{
-  RES_OK = 0,
-  RES_ERR = 1, // error
-  RES_NX = 2,  // key not found
-};
+static void do_del(std::vector<std::string> &cmd, Response &out) {
+  assert(cmd.size() == 2);
+  Entry key;
+  key.key.swap(cmd[1]);
+  key.node.hcode = str_hash((uint8_t *)key.key.data(), key.key.size());
+  HNode *node = h_lookup(&g_data.db, &key.node, entry_eq);
+  if (node) {
+    delete container_of(node, Entry, node);
+  }
+  return;
+}
 
-struct Response
-{
-  uint32_t status = 0;
-  std::vector<uint8_t> data;
-};
+static void do_set(std::vector<std::string> &cmd, Response &out) {
+  assert(cmd.size() == 3);
+  Entry key;
+  key.key.swap(cmd[1]);
+  key.node.hcode = str_hash((uint8_t *)key.key.data(), key.key.size());
+  HNode *node = h_lookup(&g_data.db, &key.node, entry_eq);
+  if (node) {
+    container_of(node, Entry, node)->val.swap(cmd[2]);
+  } else {
+    Entry *ent = new Entry();
+    ent->key.swap(key.key);
+    ent->node.hcode = key.node.hcode;
+    ent->val.swap(cmd[2]);
+    hm_insert(&g_data.db, &ent->node);
+  }
+  out.status = RES_OK;
+  return;
+}
 
-static void do_request(std::vector<std::string> &cmd, Response &out)
-{
-  if (cmd.size() == 2 && cmd[0] == "get")
-  {
-    // get from map and return
-    auto it = g_data.find(cmd[0]);
-    if (it == g_data.end())
-    {
-      out.status = RES_NX;
-    }
+static void do_get(std::vector<std::string> &cmd, Response &out) {
+  assert(cmd.size() == 2);
+  Entry key;
+  key.key.swap(cmd[1]);
+  key.node.hcode = str_hash((uint8_t *)key.key.data(), key.key.size());
+  const HNode *lookup_node = h_lookup(&g_data.db, &key.node, entry_eq);
+  if (!lookup_node) {
+    out.status = RES_NX;
     return;
   }
-  else if (cmd.size() == 3 && cmd[0] == "set")
-  {
-    g_data[cmd[1]].swap(cmd[2]);
-    return;
-  }
-  else if (cmd.size() == 2 && cmd[0] == "del")
-  {
-    g_data.erase(cmd[1]);
-  }
-  else
-  {
-    out.status = RES_ERR; // unrecognized command
-  }
+  // copy the value
+  const auto &val = container_of(lookup_node, Entry, node)->val;
+  assert(val.size() < k_max_msg);
+  out.data.assign(val.begin(), val.end());
 }
 
-static void msg(const char *msg) { fprintf(stderr, "%s\n", msg); }
-
-static void msg_errno(const char *msg)
-{
-  int err = errno;
-  fprintf(stderr, "[%d] %s\n", err, msg);
-}
-
-static void die(const char *msg)
-{
-  int err = errno;
-  fprintf(stderr, "[%d] %s\n", err, msg);
-  abort();
-}
-
-struct Conn
-{
-  int fd = -1;
-  bool want_to_read = false;
-  bool want_to_write = false;
-  bool want_to_close = false;
-  std::vector<uint8_t> incoming;
-  std::vector<uint8_t> outgoing;
-};
-
-void fd_set_nb(int fd)
-{
+void fd_set_nb(int fd) {
   errno = 0;
   int flags = fcntl(fd, F_GETFL, 0);
-  if (errno)
-  {
+  if (errno) {
     die("fcntl error");
     return;
   }
@@ -117,19 +90,16 @@ void fd_set_nb(int fd)
   flags |= O_NONBLOCK;
   errno = 0;
   (void)fcntl(fd, F_SETFL, flags);
-  if (errno)
-  {
+  if (errno) {
     die("fcntl error");
   }
 }
 
-static Conn *handle_accept(int fd)
-{
+static Conn *handle_accept(int fd) {
   struct sockaddr_in client_addr = {};
   socklen_t addrlen = sizeof(client_addr);
   int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
-  if (connfd < 0)
-  {
+  if (connfd < 0) {
     // accept the request and process it
     return NULL;
   }
@@ -142,41 +112,34 @@ static Conn *handle_accept(int fd)
 }
 
 static void buf_append(std::vector<uint8_t> &buf, const uint8_t *data,
-                       size_t len)
-{
+                       size_t len) {
   buf.insert(buf.end(), data, data + len);
 }
 
-static void buf_consume(std::vector<uint8_t> &buf, size_t n)
-{
+static void buf_consume(std::vector<uint8_t> &buf, size_t n) {
   buf.erase(buf.begin(), buf.begin() + n);
 }
 
-static void make_reponse(const Response &resp, std::vector<uint8_t> &out)
-{
+static void make_response(const Response &resp, std::vector<uint8_t> &out) {
   uint32_t resp_len = 4 + (uint32_t)out.size();
   buf_append(out, (const uint8_t *)&resp_len, 4);
 }
 
 // process 1 request if there is enough data
-static bool try_one_request_og(Conn *conn)
-{
+static bool try_one_request_og(Conn *conn) {
   // try to parse the protocol: message header
-  if (conn->incoming.size() < 4)
-  {
+  if (conn->incoming.size() < 4) {
     return false; // want read
   }
   uint32_t len = 0;
   memcpy(&len, conn->incoming.data(), 4);
-  if (len > k_max_msg)
-  {
+  if (len > k_max_msg) {
     msg("too long");
     conn->want_to_close = true;
     return false; // want close
   }
   // message body
-  if (4 + len > conn->incoming.size())
-  {
+  if (4 + len > conn->incoming.size()) {
     return false; // want read
   }
   const uint8_t *request = &conn->incoming[4];
@@ -195,10 +158,8 @@ static bool try_one_request_og(Conn *conn)
   return true; // success
 }
 
-static bool read_u32(const uint8_t *&cur, const uint8_t *end, uint32_t &out)
-{
-  if (cur + 4 > end)
-  {
+static bool read_u32(const uint8_t *&cur, const uint8_t *end, uint32_t &out) {
+  if (cur + 4 > end) {
     return -1;
   }
 
@@ -208,10 +169,8 @@ static bool read_u32(const uint8_t *&cur, const uint8_t *end, uint32_t &out)
 }
 
 static bool read_str(const uint8_t *&cur, const uint8_t *end, size_t n,
-                     std::string &out)
-{
-  if (cur + n > end)
-  {
+                     std::string &out) {
+  if (cur + n > end) {
     return -1;
   }
   out.assign(cur, cur + n);
@@ -220,44 +179,36 @@ static bool read_str(const uint8_t *&cur, const uint8_t *end, size_t n,
 }
 
 static int32_t parse_request(const uint8_t *data, size_t size,
-                             std::vector<std::string> &out)
-{
+                             std::vector<std::string> &out) {
 
   const uint8_t *end = data + size; // advance by size bytes
 
   uint32_t nstr = 0;
-  if (!read_u32(data, end, nstr))
-  {
+  if (!read_u32(data, end, nstr)) {
     return -1;
   }
 
-  if (nstr > k_max_msg)
-  {
+  if (nstr > k_max_msg) {
     return -1;
   }
 
   // append until the out is of size nstr
   // other way of saying, run the while loop for nstr times
-  while (out.size() < nstr)
-  {
+  while (out.size() < nstr) {
     uint32_t len = 0;
-    if (!read_u32(data, end, len))
-    {
+    if (!read_u32(data, end, len)) {
       return -1;
     }
-    if (len > k_max_msg)
-    {
+    if (len > k_max_msg) {
       return -1;
     }
     out.push_back(std::string());
-    if (!read_str(data, end, len, out.back()))
-    {
+    if (!read_str(data, end, len, out.back())) {
       return -1;
     }
   }
 
-  if (data != end)
-  {
+  if (data != end) {
     return -1;
   }
 
@@ -266,10 +217,8 @@ static int32_t parse_request(const uint8_t *data, size_t size,
 
 // conn.want_to_close = true => on any protocol error
 // returns true if message is processed
-static bool try_one_request(Conn *conn)
-{
-  if (conn->incoming.size() < 4)
-  {
+static bool try_one_request(Conn *conn) {
+  if (conn->incoming.size() < 4) {
     return false; // want to read more
   }
 
@@ -282,33 +231,31 @@ static bool try_one_request(Conn *conn)
     return false;
   }
 
-  if (4 + len > conn->incoming.size())
-  {
+  if (4 + len > conn->incoming.size()) {
     return false;
   }
 
   const uint8_t *request = &conn->incoming[4];
-  printf("client says: len:%d data:%.*s\n", len, len < 100 ? len : 100,
-         request);
+  std::vector<std::string> cmd;
+  if (parse_request(request, len, cmd)) {
+    Response resp;
+    do_request(cmd, resp);
+    make_response(resp, conn->outgoing);
+  }
 
-  // just echo for now
-  buf_append(conn->outgoing, (const uint8_t *)&len, 4);
-  buf_append(conn->outgoing, request, len);
-
+  // application logic done! remove the request message.
   buf_consume(conn->incoming, 4 + len);
+
   return true; // success
 }
 
-static void handle_write(Conn *conn)
-{
+static void handle_write(Conn *conn) {
   assert(conn->outgoing.size() > 0);
   size_t rv = write(conn->fd, &conn->outgoing[0], conn->outgoing.size());
-  if (rv < 0 && rv == EAGAIN)
-  {
+  if (rv < 0 && rv == EAGAIN) {
     return;
   }
-  if (rv < 0)
-  {
+  if (rv < 0) {
     msg_errno("write() error");
     conn->want_to_close = true;
     return;
@@ -322,8 +269,7 @@ static void handle_write(Conn *conn)
   return;
 }
 
-static void handle_read(Conn *conn)
-{
+static void handle_read(Conn *conn) {
   uint8_t buf[64 * 1024];
   int rv = read(conn->fd, buf, sizeof(buf));
   if (rv < 0 || rv == EAGAIN) // handle IO error -> err < 0 and err == 0 i.e EOF
@@ -336,14 +282,10 @@ static void handle_read(Conn *conn)
     conn->want_to_close = true;
     return;
   }
-  if (rv == 0)
-  {
-    if (conn->incoming.size() == 0)
-    {
+  if (rv == 0) {
+    if (conn->incoming.size() == 0) {
       msg("client closed");
-    }
-    else
-    {
+    } else {
       msg("unexpected EOF");
     }
     conn->want_to_close = true;
@@ -352,13 +294,11 @@ static void handle_read(Conn *conn)
   buf_append(conn->incoming, buf, (size_t)rv);
 
   // this is critical to the pipelined request handling
-  while (try_one_request(conn))
-  {
+  while (try_one_request(conn)) {
     printf("processed one request from conn %d\n", conn->fd);
   }
 
-  if (conn->outgoing.size() > 0)
-  {
+  if (conn->outgoing.size() > 0) {
     conn->want_to_write = true;
     conn->want_to_read = false;
     return handle_write(conn);
@@ -367,11 +307,9 @@ static void handle_read(Conn *conn)
   return;
 }
 
-int main()
-{
+int main() {
   int fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (fd < 0)
-  {
+  if (fd < 0) {
     die("socket()");
   }
   int val = 1;
@@ -383,8 +321,7 @@ int main()
   addr.sin_port = ntohs(1234);
   addr.sin_addr.s_addr = ntohl(0); // wildcard address 0.0.0.0
   int rv = bind(fd, (const sockaddr *)&addr, sizeof(addr));
-  if (rv)
-  {
+  if (rv) {
     die("bind()");
   }
 
@@ -394,8 +331,7 @@ int main()
   // listen
   fprintf(stderr, "trying to listen\n");
   rv = listen(fd, SOMAXCONN);
-  if (rv)
-  {
+  if (rv) {
     die("listen()");
   }
   fprintf(stderr, "server FD = %d\n", fd);
@@ -404,51 +340,41 @@ int main()
   std::vector<struct pollfd> poll_args;
 
   fprintf(stderr, "started listening....\n");
-  while (true)
-  {
+  while (true) {
     poll_args.clear();
     struct pollfd pfd = {fd, POLLIN, 0};
     poll_args.push_back(pfd);
 
-    for (Conn *conn : fd2conn)
-    {
-      if (!conn)
-      {
+    for (Conn *conn : fd2conn) {
+      if (!conn) {
         continue;
       }
 
       struct pollfd pfd = {conn->fd, POLLERR, 0};
-      if (conn->want_to_read)
-      {
+      if (conn->want_to_read) {
         pfd.events |= POLLIN;
       }
-      if (conn->want_to_write)
-      {
+      if (conn->want_to_write) {
         pfd.events |= POLLOUT;
       }
       poll_args.push_back(pfd);
     }
 
     int rv = poll(poll_args.data(), (nfds_t)poll_args.size(), -1);
-    if (rv < 0 && errno == EINTR)
-    {
+    if (rv < 0 && errno == EINTR) {
       fprintf(stderr, "poll returned - %d, errno - %d, trying again\n", rv,
               errno);
       continue;
     }
-    if (rv < 0)
-    {
+    if (rv < 0) {
       fprintf(stderr, "poll returned - %d", rv);
       die("poll");
     }
 
     // We set POLLIN for server fd, so now we have connections to process
-    if (poll_args[0].revents)
-    {
-      if (Conn *conn = handle_accept(fd))
-      {
-        if (fd2conn.size() <= (size_t)conn->fd)
-        {
+    if (poll_args[0].revents) {
+      if (Conn *conn = handle_accept(fd)) {
+        if (fd2conn.size() <= (size_t)conn->fd) {
           fd2conn.resize(conn->fd + 1);
         }
         fd2conn[conn->fd] = conn;
@@ -458,34 +384,28 @@ int main()
     // handle connection sockets
     // go over all known connections
     // and check if they have revents
-    for (size_t i = 1; i < fd2conn.size(); ++i)
-    {
+    for (size_t i = 1; i < fd2conn.size(); ++i) {
       uint32_t ready = poll_args[i].revents;
-      if (ready == 0)
-      {
+      if (ready == 0) {
         continue;
       }
 
       Conn *conn = fd2conn[poll_args[i].fd];
-      if (!conn)
-      {
+      if (!conn) {
         die("connection is nil");
       }
       // check if they are POLLIN or POLLOUT or both
-      if (ready & POLLIN)
-      {
+      if (ready & POLLIN) {
         assert(conn->want_to_read);
         handle_read(conn);
       }
 
-      if (ready & POLLOUT)
-      {
+      if (ready & POLLOUT) {
         assert(conn->want_to_write);
         handle_write(conn);
       }
 
-      if ((ready & POLLERR) || conn->want_to_close)
-      {
+      if ((ready & POLLERR) || conn->want_to_close) {
         (void)close(conn->fd);
         fd2conn[conn->fd] = NULL;
         delete conn;
